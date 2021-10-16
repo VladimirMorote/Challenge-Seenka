ce3ff317ac2bfe5088c9bc4b872f6cd45390ff1d
Vladimir Morote <46836725+VladimirMorote@users.noreply.github.com>
2021-10-16 03:25:36 +0000

    add

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ texto = "La historia de la opera tiene una dura realidad en nuestra nacion"
 ancho = 30
 
 def justificar(texto,ancho):
-  linea =  ""
+  linea = ""
+  resto = ""
   i = 1
   for letra in texto:
-    linea += letra
-    if letra != " ":
-      resto += letra
-    else:
-      resto = ""
+    if i < 30:
+      linea += letra
+      if letra != " ":
+        resto += letra
+      else:
+        resto = ""
       i += 1
     elif letra == " ":
       linea = linea.strip()
