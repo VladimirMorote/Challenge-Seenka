4a59956c16fe7dc5374f9c78785298d7c1a9b095
Vladimir Morote <46836725+VladimirMorote@users.noreply.github.com>
2021-10-17 12:45:50 +0000

    ultimo

--- Ejercicio.py
+++ Ejercicio.py
@@ -11,39 +11,39 @@ def justificar(texto,tam):
     resultado = ""
     for letra in texto:
         if i < 30:
-            '''Se arma la línea hasta el caracter 30'''
+            #Se arma la línea hasta el caracter 30
             linea += letra
             if letra != " ":
-                '''En la variable resto se va guardando cada palabra que se suma a la línea'''
+                #En la variable resto se va guardando cada palabra que se suma a la línea
                 resto += letra
             else:
-                '''Si ya la palabra se cargó en la línea, la variable resto se vacía para guardar la palagra siguiente'''
+                #Si ya la palabra se cargó en la línea, la variable resto se vacía para guardar la palagra siguiente
                 resto = ""
             i += 1
         elif letra == " ":
-            '''Si el último caracter de la línea es un espacio, se termina la línea'''
+            Si el último caracter de la línea es un espacio, se termina la línea
             linea = linea.strip()
-            '''Se borra el último caracter'''
+            #Se borra el último caracter
             lista = linea.split()
             if len(linea) == 30:
-                '''Acá se pregunta si la línea es justo de 30 caracteres con la línea completa.
-                Se puede dar que casualmente la línea no necesite ser justificada.'''
+                #Acá se pregunta si la línea es justo de 30 caracteres con la línea completa
+                #Se puede dar que casualmente la línea no necesite ser justificada
                 resultado += linea + "\n"
                 linea = ""
                 i = 1
             else:
-                '''Se pasa la linea a un formato de lista'''
+                #Se pasa la linea a un formato de lista
                 lista.insert(1," ")
-                '''Se inserta un espacio después de la primer palabra'''
+                #Se inserta un espacio después de la primer palabra
                 linea = ""
                 for palabra in lista:
-                    '''En este for se arma la línea correcta a partir de la lista y con el espacio que se insertó'''
+                    #En este for se arma la línea correcta a partir de la lista y con el espacio que se insertó
                     if palabra != " ":
                         linea += palabra + " "
                     else:
                         linea += palabra
                 linea = linea.strip()
-                '''Línea terminada'''
+                #Línea terminada
                 resultado += linea + "\n"
                 linea = ""
                 i = 1
@@ -55,16 +55,16 @@ def justificar(texto,tam):
             linea = linea.strip()
             '''Se elimina el espacio en blanco al final de la línea'''
             lista = linea.split()
-            '''Se pasa la linea a un formato de lista'''
+            #Se pasa la linea a un formato de lista
             espacios = 30 - len(linea)
-            '''Se calcula los espacios en blanco que hay que agregar a la línea para que llegue a 30 caracteres.'''
+            #Se calcula los espacios en blanco que hay que agregar a la línea para que llegue a 30 caracteres
             ratio = espacios / (len(lista) - 1)
-            '''Se calcula cuantos espacios en blanco van entre cada palabra'''
+            #Se calcula cuantos espacios en blanco van entre cada palabra
             if ratio > 1:
-                '''Si el ratio es mayor a 1, se tiene que agregar al menos 1 espacio entre cada palabra'''
+                #Si el ratio es mayor a 1, se tiene que agregar al menos 1 espacio entre cada palabra
                 if ratio % (len(lista) - 1) == 0:
-                    '''Si el módulo del ratio por la cantidad de palabras es 0, quiere decir que entre palabra 
-                    y palabra tiene que haber exactamente el mismo espacio.'''
+                    #Si el módulo del ratio por la cantidad de palabras es 0, quiere decir que entre palabra 
+                    #y palabra tiene que haber exactamente el mismo espacio
                     linea = ""
                     for palabra in lista:
                         linea += palabra + " "
@@ -72,8 +72,8 @@ def justificar(texto,tam):
                             linea += " "
                     linea = linea.strip()
                 else:
-                    '''Si el módulo del ratio por la cantidad de palabras no es 0, quiere decir que las primeras
-                    palabras de la línea van a tener más espacios en blanco que otras.'''
+                    #Si el módulo del ratio por la cantidad de palabras no es 0, quiere decir que las primeras
+                    #palabras de la línea van a tener más espacios en blanco que otras.
                     linea = ""
                     restantes = espacios % (len(lista) - 1)
                     i = 0
@@ -87,7 +87,7 @@ def justificar(texto,tam):
                             restantes -= 1
                     linea = linea.strip()
             else:
-                '''Si el ratio es menor que 1, solo se tienen que agregar algunos espacios en blanco en la línea'''
+                #Si el ratio es menor que 1, solo se tienen que agregar algunos espacios en blanco en la línea
                 linea = ""
                 i = 0
                 for palabra in lista:
@@ -107,7 +107,7 @@ def justificar(texto,tam):
     return resultado
 
 
-'''Se llama a la función con el texto a justificar y el tamaño de línea'''
+#Se llama a la función con el texto a justificar y el tamaño de línea
 print("---PRIMER PRUEBA---")
 print("")
 a = justificar(texto,tam)
